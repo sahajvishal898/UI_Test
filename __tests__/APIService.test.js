2e8fb09c76d9f3f015e99e1a965db1f9874a853d
Pranav-Bhosale <pranavbhosale@sahaj.ai>
2023-03-21 11:42:48 +0530

    Add stub for APIService

--- __tests__/APIService.test.js
+++ __tests__/APIService.test.js
@@ -1,44 +1,16 @@
-import ViewOrderService from '../src/viewOrderHistory/ViewOrderService';
-//import AddOrder from '../src/addOrder/AddOrder';
 
+import APIService from "../src/APIService";
 
-var route='http://localhost:8080/user/atu_1/order'
+class fetchStub {
+  get(path) {
+    return Promise.reject("Something happened");
+  }
+}
 
-describe('APIService test', () => {
-  let apiService;
+it("thrpow error", () => {
+  const apiService = new APIService(new fetchStub);
 
-  // it('should throw error when fetch throws error', () => {
-  //   apiService = new APIService(new FailureStub());
-  //   apiService.makeGetRequest('someRoute').catch((err) => {
-  //       expect(err).toBeTruthy();
-  //   });
-  // });
-
-  // it('should work when axios works', () => {});
-
-  it('should throw error when fetch throws error', () => {
-    
-   let viewOrderServiceStub = new ViewOrderService()
-
-    let content = {
-      error: ["error got"]
-    }
-
-    let error=viewOrderServiceStub.viewOrderServiceFetch().catch((err) => {
-      expect(JSON.stringify(content)).toEqual(err);
-
-    })
-
-  });
-
-  it('should send true', function(){
-    expect(true).toBe(true)
-  })
-
-
-
-
-
+  expect(apiService.makeGetRequest(" ")).rejects.toEqual("Something happened");
 });
 
 
